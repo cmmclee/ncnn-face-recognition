0a09b7dfbec754e660b98702ec6352cc99e4931c
Guo Feng <feng.guo@nxp.com>
2019-03-26 10:38:37 +0800

    Add test script

--- test.py
+++ test.py
@@ -1,28 +1,12 @@
+import cv2
+import facerecognition
 import numpy as np
-import facerecognition, cv2
 
-class call_cpp(object):
-    def __init__(self, name):
-        self.itcom = mobilefacenet.communication(name)
+img = cv2.imread("Aaron_Sorkin_0001.bmp")
+facerecg = facerecognition.FaceRecognition("./models", 0.70)
 
-    def run(self, img) :
-        ret_list = self.itcom.test(img.shape[0], img.shape[1], image_char)
-        gray = np.zeros([img.shape[0], img.shape[1]], np.ubyte)
-        c = 0
-        for i in range(img.shape[0]):
-            for j in range(img.shape[1]):
-                gray[i][j] = ret_list[c]
-                c += 1
-        return gray
 
-    def run_class(self):
-        return self.itcom.ret_box_list()
+image_char = img.astype(np.uint8).tostring()
 
-if __name__ == '__main__':
-    facerecog = facerecognition.FaceRecognition("../models/")
-
-    img = cv2.imread('6.png')
-
-    image_char = img.astype(np.uint8).tostring()
-    #facerecog.recognize(img.shape[0], img.shape[1], image_char)
-    facerecog.add_person("gf", img.shape[0], img.shape[1], image_char)
+rets = facerecg.recognize(img.shape[0], img.shape[1], image_char)
+print(rets)
